6de74a3b9330b765edf73348c85f75cb0eb74a52
Kishan Kumar <kishan.kumar@zimtive.com>
2023-01-23 18:21:38 +0530

    updated

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import emojipedia from "../emojipedia";
+import Entry from "./Entry";
+
+const createEntry = (emojiTerm) => {
+    return(
+        <Entry
+            key={emojiTerm.id} 
+            emoji={emojiTerm.emoji}
+            name={emojiTerm.name}
+            description={emojiTerm.meaning}
+        />
+    );
+}
+
+const App = () => {
+    return(
+        <div>
+            <h1>
+                <span>emojipedia</span>
+            </h1>
+            <dl className="dictionary">
+                {emojipedia.map(createEntry)}
+            </dl>
+        </div>
+    );
+}
+
+export default App;
